d07c8753045dbecf30454aefcc512a84df209013
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2024-08-28 22:40:02 +0200

    cleanup

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
 	$('form.variations_form').on('found_variation', function(event, variation) {
-		// Twój kod, który ma zostać wykonany po zmianie wariantu
+
 		const $lowestPricePlaceholder = $( '.wc-price-history.prior-price-value .wc-price-history.prior-price.lowest-raw-value'),
 		  lowestInVariation = variation._wc_price_history_lowest_price;
 
@@ -16,7 +16,6 @@ jQuery(document).ready(function($) {
 
 		let formattedPrice = parseFloat( price ).toFixed( wc_price_history_frontend.decimals );
 
-		// Zamień separator dziesiętny na zdefiniowany w WooCommerce
 		formattedPrice = formattedPrice.replace(',', wc_price_history_frontend.thousand_separator);
 		formattedPrice = formattedPrice.replace('.', wc_price_history_frontend.decimal_separator);
 
